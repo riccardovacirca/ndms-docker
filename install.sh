8e1cf33f4da8ec224d46cb31a8cb254e9c0b61d1
riccardo.vacirca <vacirca@olomedia.it>
2024-12-08 22:15:00 +0100

    updated

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-docker-compose -f docker-compose.dev.yml up --build
 git clone https://github.com/riccardovacirca/ndms-module.git module/ndms
 cd module/ndms && npm link
 rm -rf module/.gitkeep
