0edf80d030caabe9ef7107ed5038a24e254289e8
riccardo.vacirca <vacirca@olomedia.it>
2024-12-09 18:48:03 +0100

    updated

--- install-module.sh
+++ install-module.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mkdir -p modules
 git clone https://github.com/riccardovacirca/ndms-module.git modules/ndms
-cd modules/ndms && npm link
+cd modules/ndms && npm install && npm link
 cd /service && npm link ndms-module
